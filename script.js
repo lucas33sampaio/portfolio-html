1d914b95eb5de1797cbcef415dc76d31db61d97f
Lucas Sampaio <lucas_a.sampaio@live.com>
2026-06-04 18:45:46 -0300

    Alternancia de nome e cor em um botão e ocultar e mostrar lista de Tecnologias

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Projeto funcionando!");
+let NomeTitular = document.getElementById("nome");
+NomeTitular.style.color = "black";
+NomeTitular.innerText = "Lucas Sampaio";
+let botaoMudar = document.getElementById("botaoNome");
+let botaoEsconderTec = document.getElementById("botaoTecnologias");
+let listaTec = document.getElementById("listaTecnologias")
+listaTec.style.display = "none"
+
+botaoMudar.addEventListener("click", function(){
+    if (NomeTitular.style.color === "black") {
+        NomeTitular.innerText = "Lucas";
+        NomeTitular.style.color = "red";
+    } 
+    else {
+        NomeTitular.innerText = "Lucas Sampaio";
+        NomeTitular.style.color = "black";
+    }
+    console.log(NomeTitular.style.color);
+});
+
+botaoEsconderTec.addEventListener("click", function(){
+    if (listaTec.style.display === "none") {
+        listaTec.style.display = "block" 
+    }
+    else {
+        listaTec.style.display = "none"
+    }
+});
